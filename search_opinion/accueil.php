aebe98779d2daff3934a3784971d60de81b31432
ach-ber <achilleberpgmail.com>
2021-02-19 14:12:03 +0100

    page avis

--- search_opinion/accueil.php
+++ search_opinion/accueil.php
@@ -8,54 +8,106 @@
 
 <body>
 	
+	
 	<header>
-		<div id="bandeau_contact">
-			<a href="">Contact</a>
+		<div id="header_logo">
+				<img src="image/Logo_Polytech_5.png">
 		</div>
 
-		<div id="bandeau_logo">
-			<img src="Logo_Polytech_2.png">
-		</div>
-		<div id="bandeau_Avis">
-			<a href="">Publier un avis</a>
+		<div id="header_contact"><a href="">Contact<img src="image/index.png"></a></div>
+
+		<div id="header_Publier">
+			<a href=""><img src="image/+_1.png"> Publier</a>
 		</div>
 
-		<div id="bandeau_Connexion">
-			<a href="">Connexion</a>
-		</div>
-		<div id="bandeau_Compte">
-			<a href="">Céer un compte</a>
+		<div id="header_Connexion">
+			<a href="">Connexion<img src="image/index.png"></a>
 		</div>
 		
-		
+		<div id="header_Compte">
+			<a href="">Inscription<img src="image/index.png"></a>
+		</div>
 	</header>
 
-	<div id="page_accueil">
-		<div id="grandtitre">Stage &<br /> avis</div>
+	<div id="fond_accueil">
+		<h1>Stage & avis</h1>
 		<h2>Rechercher les stages selon des critères<br>Et selon d'autres critères</h2>
-		<div id="barre_recherche">
+		<div id="recherche_global">
 			<form action="recherche.php" method="post">
-	 			<input type="text" name="recherche" size="30" list="liste_all" placeholder="Recherche" />
-	 			<datalist id="liste_all">
-	 				<!-- Connexion à la base de données -->
-					<?php include("connexion_bdd.php");
-					// Sélection des titres de musique, nom des artistes, nom des pays et nom des styles musicaux pour faciliter la recherche
-						$lieux = $bdd->query('SELECT lieu FROM localisation');
-						echo '<option value="Toute localisation">';
-						while($lieu = $lieux->fetch()) {
-							echo '<option value="' . $lieu['lieu'] . '">';
-						}
-						$lieux->closeCursor(); ?>
-				</datalist>
-	 			<input type="submit" name="envoyer" value="&#x2713;" />
-	 		</form>
+				<div id="recherche">
+					<div id="recherche_localisation">Localisation<img src="image/logo_localisation_2.png"></div>
+					<input type="text" name="recherche" size="30" list="liste_all"placeholder="ex : Paris">
+
+				</div>
+				<div id="recherche_2">
+					<div id="recherche_domaine">Domaine<img src="image/logo_domaine.png"></div>
+					<input type="text" name="" placeholder="ex : informatique">
+				</div>
+				<a href="">
+				<div id="recherche_3"><div id="recherche_final"><img src="image/logo_loupe.png"></div><div id="recherche_fleche_3"></div></div>
+				</a>
+			</form>
 		</div>
-
-		<div id="slide2"></div>
-
+		
 	</div>
+
+	<aside>
+		<h2>Retrouvez les stages les mieux notés<br>& vos dernières recherches</h2>
+	</aside>
+
+	<section>
+
+		<div id="stage_populaire">
+
+			<div class="accueil_stage">
+				<div class="accueil_stage_logo"><img src="image/logo_accueil.webp"></div>
+				<div class="accueil_stage_etoile"><img src="image/etoile_1.png"><img src="image/etoile_1.png"><img src="image/etoile_1.png"><img src="image/etoile_2.png"><img src="image/etoile_2.png"></div>
+				<div class="accueil_stage_info"><img src="image/logo_domaine_black.png">Informatique</div>
+				<div class="accueil_stage_info"><img src="image/logo_localisation_black.png">Lyon</div>
+				<h1>Tesla</h1>
+				<p><img src="image/guillemet_inv.png">Encore un extrait du début de l'avis sur un autre stage pour test. Dans l'idéale on met là aussi 4-5 lignes pour donner envie de lire la suite ...<img src="image/guillemet.png"></p>
+				<time>14/02/2020</time>
+			</div>
+
+			<div class="accueil_stage">
+				<div class="accueil_stage_logo"><img src="image/generale.png"></div>
+				<div class="accueil_stage_etoile"><img src="image/etoile_1.png"><img src="image/etoile_1.png"><img src="image/etoile_1.png"><img src="image/etoile_2.png"><img src="image/etoile_2.png"></div>
+				<div class="accueil_stage_info"><img src="image/logo_domaine_black.png">Génie civil</div>
+				<div class="accueil_stage_info"><img src="image/logo_localisation_black.png">Bordeaux</div>
+				<h1>Société générale</h1>
+				<p><img src="image/guillemet_inv.png">Extrait du début de l'avis sur le stage. Dans l'idéale on met 4-5 lignes pour donner envie de lire la suite ...<img src="image/guillemet.png"></p>
+				<time>14/02/2020</time>
+			</div>
+
+			<div class="accueil_stage"></div>
+			<div class="accueil_stage"></div>
+			<div class="accueil_stage"></div>
+			
+		</div>
+			
+		<div id="stage_visite">
+			<div class="accueil_stage" id="stage_info">
+				<h2>Plus d'infos avec des liens qui renvoie à des pages</h2>
+				<a href="">Des infos</a>
+				<a href="">D'autres infos</a>
+			</div>
+			<div class="accueil_stage"></div>
+			<div class="accueil_stage"></div>
+			<div class="accueil_stage"></div>
+			<div class="accueil_stage"></div>
+		</div>
+	</section>
+	<footer>
+		
+	</footer>
 
 	
 </body>
-	
 </html>
+
+
+			
+
+	 			
+	 		
+	<input type="submit" name="envoyer" value="&#x2713;" />
--- search_opinion/connexion_bdd.php
+++ search_opinion/connexion_bdd.php
@@ -1,8 +1,8 @@
 <?php
 try {
-	$bdd = new PDO('mysql:host=localhost;dbname=site_polytech;charset=utf8', 'root', '');
-	$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	$bdd = new PDO('mysql:host=localhost;port=3308;dbname=bdsite;charset=utf8', 'root', ''array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
+
 catch(Exception $e) {
 	die('Erreur : '.$e->getMessage());
 } ?>
