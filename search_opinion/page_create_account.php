44d2dbad7f5031b93f489512086abca396f76515
LeaVerdi <77842823+LeaVerdi@users.noreply.github.com>
2021-02-26 15:46:38 +0100

    create_account

--- search_opinion/page_create_account.php
+++ search_opinion/page_create_account.php
@@ -3,7 +3,7 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8"/>
-	<link  href="avis.css" rel="stylesheet" type="text/css" media="all">
+	<link  href="autre.css" rel="stylesheet" type="text/css" media="all">
 	<title>Dépôt d'un avis</title>
 </head>
 <body>
@@ -28,7 +28,7 @@
 	</header>
 	<div id="create_account">
 		<form action="script_create_account.php" method="POST" >
-			<div><label><div class="label">Adresse mail (étudiante): <strong id="interrogation">?</strong><div id="text_info">Nous vous conseillons de rediriger votre adresse mail étudiante vers une boîte mail personnelle pour simplifier les échanges. Vous pouvez faire cela sur CELENE via l'onglet "Redirection boîte mail".</div></div><input class="formule" id="mail" type="text" name="mail" pattern="[a-z0-9\.-]{3,}" minlength="5" maxlength="35" placeholder=" ex : prenom.nom" title="Votre adresse mail" required autofocus>@etu.univ-tours.fr</label></div>
+			<div><label><div class="label">Adresse mail (étudiante): <span id="interrogation" data-descr="Nous vous conseillons de rediriger votre adresse mail étudiante vers une boîte mail personnelle pour simplifier les échanges. Vous pouvez faire cela sur CELENE via l'onglet &quot;Redirection boîte mail&quot;.">?</span></div><input class="formule" id="mail" type="text" name="mail" pattern="[a-z0-9\.-]{3,}" minlength="5" maxlength="35" placeholder=" ex : prenom.nom" title="Votre adresse mail" required autofocus>@etu.univ-tours.fr</label></div>
 			<div><label><div class="label">Nom : </div><div id="auto_nom"></div><input class="formule" id="nom" type="text" name="nom" placeholder="Nom" title="Votre nom" ></label></div>
 			<div><label><div class="label">Prénom : </div><div id="auto_prenom"></div><input class="formule" id="prenom" type="text" name="prenom" placeholder="Prénom" title="Votre prénom" ></label></div>
 			<div><label><div class="label">Mot de passe : </div><input class="formule" type="password" name="mdp" id="mdp" placeholder="Mot de passe" title="Créez un mot de passe" required></label></div>
