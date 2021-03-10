7c0d8f7b0b0beb087055c7f0ec2aed6c08b3a762
LeaVerdi <77842823+LeaVerdi@users.noreply.github.com>
2021-03-10 19:06:12 +0100

    Update connexion_bdd.php

--- search_opinion/connexion_bdd.php
+++ search_opinion/connexion_bdd.php
@@ -1,6 +1,6 @@
 <?php
 try {
-	$bdd = new PDO('mysql:host=localhost;port=3306;dbname=bdsite;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;port=3306;dbname=site_polytech;charset=utf8', 'root', '');
 	$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 }
 
