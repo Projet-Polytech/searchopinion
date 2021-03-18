7a160f650d3a4c0744a3196bf6b4d3098de51645
ach-ber <77842805+ach-ber@users.noreply.github.com>
2021-03-18 23:38:06 +0100

    qfdvqsv
    svsvdqsvd

---
+++ search_opinion/avis_detail.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title></title>
+    <meta charset="UTF-8" >
+    <link  href="avis_detail.css" rel="stylesheet">
+</head>
+	
+<body>
+
+ 	<header>
+
+ 		<div id="header_logo">
+				<img src="image/Logo_Polytech_5.png">
+		</div>
+
+ 		<div id="header_contact"><a href="">Contact<img src="image/index.png"></a></div>
+		
+		<div id="header_Compte">
+			<a href="">Inscription<img src="image/index.png"></a>
+		</div>
+
+		<div id="header_Connexion">
+			<a href="">Connexion<img src="image/index.png"></a>
+		</div>
+
+		<div id="header_Publier">
+			<a href=""><img src="image/+_1.png"> Publier</a>
+		</div>
+		
+		
+ 	</header>
+ 	<aside>
+
+ 	<a href=""><img src="image/retour2.png">Résultats précedents</a>
+
+ 	<p>Italie<img src="image/index_2.png">Environement<img src="image/index_2.png">Nom du stage</p>
+
+ 	
+
+ 	</aside>
+ 	<div id="leftside">
+ 	</div>
+ 	<div id="rightside">
+ 	</div>
+ 	<section>
+
+ 		<div id="detail_left">
+ 			<img src="image/generale.png">
+ 			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9508.996379979799!2d-0.5406837830637089!3d44.8818036295758!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5528dfc3af92df%3A0xb86c663b296b5f78!2zU29jacOpdMOpIEfDqW7DqXJhbGUgZGVzIEJvaXM!5e0!3m2!1sfr!2sfr!4v1613683502141!5m2!1sfr!2sfr"  frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+ 		</div>
+ 		<div id="detail_right">
+ 			<h1>Titre de stage assez long</h1>
+ 			<p>11/03/2021</p>
+ 			<div id="info_detail">
+ 				<div><img src="image/logo_domaine_black.png">Informatique industriel</div>
+ 				<div><img src="image/logo_billet_2.png">1800 €</div>			
+ 				<div><img src="image/calendrier.png">12 semaines</div>
+ 				<div><img src="image/logo_localisation_black.png">République démocratique du Congo</div>
+ 			</div>
+
+ 			<div id="etoile_detail">
+ 				<div id="details_interet">
+ 					<div class="avis_stage_etoile"><img src="image/etoile_full.png"><img src="image/etoile_full.png"><img src="image/etoile_full.png"><img src="image/etoile_demi.png"><img src="image/etoile_null.png"></div>interet
+ 				</div>
+ 				<div id="details_ambiance">
+ 					<div class="avis_stage_etoile"><img src="image/etoile_full.png"><img src="image/etoile_full.png"><img src="image/etoile_full.png"><img src="image/etoile_demi.png"><img src="image/etoile_null.png"></div>ambiance
+ 				</div>
+ 				<div id="details_accessibilite">
+ 					<div class="avis_stage_etoile"><img src="image/etoile_full.png"><img src="image/etoile_full.png"><img src="image/etoile_full.png"><img src="image/etoile_demi.png"><img src="image/etoile_null.png"></div>accessiblité
+ 				</div>
+ 				<div id="details_encadrement">
+ 					<div class="avis_stage_etoile"><img src="image/etoile_full.png"><img src="image/etoile_full.png"><img src="image/etoile_full.png"><img src="image/etoile_demi.png"><img src="image/etoile_null.png"></div>encadrement
+ 				</div>
+ 				
+ 			</div>
+ 			<p>
+ 				<img src="image/guillemet_inv.png">
+ 				Encore un extrait du début de l'avis sur un autre stage pour test. Dans l'idéale on met là aussi 4-5 lignes pour donner envie de lire la suite. Cette fois extrait plus long donc au moins 10 lignes pour que ça remplisse Vous êtes à la recherche d’une offre de stage ? StudentJob vous propose de nombreuses annonces de stage dans la vente 
+
+ 				Encore un extrait du début de l'avis sur un autre stage pour test. Dans l'idéale on met là aussi 4-5 lignes pour donner envie de lire la suite. Cette fois extrait plus long donc au moins 10 lignes pour que ça remplisse Vous êtes à la recherche d’une offre de stage ? StudentJob vous propose de nombreuses annonces de stage dans la vente.Encore un extrait du début de l'avis sur un autre stage pour test. Dans l'idéale on met là aussi 4-5 lignes pour donner envie de lire la suite. Cette fois extrait plus long donc au moins 10 lignes pour que ça remplisse Vous êtes à la recherche d’une offre de stage ? StudentJob vous propose de nombreuses annonces de stage dans la vente 
+
+ 				Encore un extrait du début de l'avis sur un autre stage pour test. Dans l'idéale on met là aussi 4-5 lignes pour donner envie de lire la suite. Cette fois extrait plus long donc au moins 10 lignes pour que ça remplisse Vous êtes à la recherche d’une offre de stage ? StudentJob vous propose de nombreuses annonces de stage dans la vente 
+
+ 				<img src="image/guillemet.png">
+ 			</p>
+ 			<a href="mailto:achilleber@gmail.com"><img src="image/enveloppe.png">Plus d'info</a>
+ 		</div>
+ 		
+ 	</section>
+ 	<footer></footer>
+ 
+
+</body>
+</html>
