cc4c3275e1454f93db3bc45d565e0a96f82f6185
LeaVerdi <77842823+LeaVerdi@users.noreply.github.com>
2021-04-01 17:57:44 +0200

    Update footer.php

--- search_opinion/footer.php
+++ search_opinion/footer.php
@@ -1,6 +1,6 @@
 <footer id="footer">
  	<h2>Nous Contacter</h2>
- 	<p>Laissez-nous votre avis à propos du site<br>Liste des réseaux disponibles</p>
+ 	<p>Laissez-nous votre avis à propos du site</br>Liste des réseaux disponibles</p>
  	<div id="imgfooter">
  		<a href=""><img src="../search_opinion/image/logo_gmail.png"></a>
  		<a href=""><img src="../search_opinion/image/logo_twitter.png"></a>
